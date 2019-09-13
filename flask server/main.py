949914da828ed3e32985f08517c56025f35eb5a8
John Silvan <johnsilvan70@gmail.com>
2019-09-13 14:52:28 +0530

    updated flash code with other prediction

--- flask server/main.py
+++ flask server/main.py
@@ -1,31 +1,78 @@
 import os
 import io
 import base64
-import pandas as pd
 from flask import Flask, jsonify, request,render_template
-import matplotlib.pylab as plt
+from matplotlib import pyplot as plt
 import base64
 import pickle
 
 app = Flask(__name__)
 
-@app.route('/',methods=['GET'])
-def getPredictions():
+
+
+@app.route('/Housing',methods=['GET'])
+def getHousingPredictions():
     #Load the saved model
     print("Loading the model...")
     loaded_model = None
-    clf = 'model_v1.pk'
+    months_ahead = 79 + int(request.args['months'])
+    clf = 'Housing.pk'
     with open(clf,'rb') as f:
         loaded_model = pickle.load(f)
     print("The model has been loaded...doing predictions now...")
     img = io.BytesIO()
     fig = plt.figure()
-    fig = loaded_model.plot_predict(1,95)
+    fig = loaded_model.plot_predict(1,months_ahead)
     fig.savefig(img, format='png')
     plt.close()
     img.seek(0)
     graph_url = base64.b64encode(img.getvalue()).decode()
     graph_url = 'data:image/png;base64,{}'.format(graph_url)
-    return render_template('index.html', graph = graph_url)
+    return render_template('index.html', graph = graph_url, name = "Housing")
+
+@app.route('/Intoxicants',methods=['GET'])
+def getIntoxicantsPredictions():
+    #Load the saved model
+    print("Loading the model...")
+    loaded_model = None
+    months_ahead = 79 + int(request.args['months'])
+    clf = 'intoxicants.pk'
+    with open(clf,'rb') as f:
+        loaded_model = pickle.load(f)
+    print("The model has been loaded...doing predictions now...")
+    img = io.BytesIO()
+    fig = plt.figure()
+    fig = loaded_model.plot_predict(1,months_ahead)
+    fig.savefig(img, format='png')
+    plt.close()
+    img.seek(0)
+    graph_url = base64.b64encode(img.getvalue()).decode()
+    graph_url = 'data:image/png;base64,{}'.format(graph_url)
+    return render_template('index.html', graph = graph_url, name = "Intoxicants")
+
+@app.route('/Food',methods=['GET'])
+def getFoodPredictions():
+    #Load the saved model
+    print("Loading the model...")
+    loaded_model = None
+    months_ahead = 79 + request.args['months']
+    clf = 'Food.pk'
+    with open(clf,'rb') as f:
+        loaded_model = pickle.load(f)
+    print("The model has been loaded...doing predictions now...")
+    img = io.BytesIO()
+    fig = plt.figure()
+    fig = loaded_model.plot_predict(1,months_ahead)
+    fig.savefig(img, format='png')
+    plt.close()
+    img.seek(0)
+    graph_url = base64.b64encode(img.getvalue()).decode()
+    graph_url = 'data:image/png;base64,{}'.format(graph_url)
+    return render_template('index.html', graph = graph_url, name = "Food and Beverages")
+
+@app.route('/')
+def home():
+    return 'CPI Prediction'
+
 if __name__ == '__main__':
-    app.run()
+    app.run(debug=True)
